5f940d78d3c023da5b41b9a7f781c9a87b9961cf
ManojAnnam <annam.manoj1996@gmail.com>
2020-01-01 23:05:18 +0530

    KnapsackLight Solution

---
+++ KnapsackLight/KnapsackLightSolution/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace KnapsackLightSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = CalculateKnapsackLight(0, 0, 0, 0, 0);
+            Console.WriteLine("Hello World!");
+        }
+
+        private static int CalculateKnapsackLight(int value1, int weight1, int value2, int weight2, int maxW)
+        {
+            if (weight1 + weight2 <= maxW)
+            {
+                return value1 + value2;
+            }
+            else if (weight1 > maxW && weight2 > maxW)
+            {
+                return 0;
+            }
+            else if ((value1 >= value2 && weight1 <= maxW) || weight2 > maxW)
+            {
+                return value1;
+            }
+            else
+            {
+                return value2;
+            }
+        }
+
+    }
+}
