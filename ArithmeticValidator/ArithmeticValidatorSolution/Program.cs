f6d2b802ebb00c11b65df1c9685ff05617d971e0
ManojAnnam <annam.manoj1996@gmail.com>
2020-01-06 09:53:06 +0530

    Arithmetic Expression Solution

---
+++ ArithmeticValidator/ArithmeticValidatorSolution/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ArithmeticValidatorSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool result = ArithmeticExpression(2, 3, 4);
+        }
+
+        public static bool ArithmeticExpression(int a, int b, int c)
+        {
+            if (a + b == c || a - b == c || a * b == c)
+            {
+                return true;
+            }
+            else
+            {
+                double temp = (a / (double)b);
+                if (Math.Ceiling(temp) == Math.Floor(temp))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+    }
+}
